e5bed9757f8086790012ed0758edf93447f1bde3
Mohudoy <mohudoy@gmail.com>
2026-08-07 23:42:22 +0600

    Complete Day 5: Functions and calculator

--- day5/function2.py
+++ day5/function2.py
@@ -1,5 +1,16 @@
-first_number = int(input("Enter the first number: "))
-second_number = int(input("Enter the second number: "))
+
+try:
+    first_number = float(input("Enter the first number: "))
+except ValueError:
+    print("You need to enter number only.")
+    raise SystemExit
+
+try:
+    second_number = float(input("Enter the second number: "))
+except ValueError:
+    print("You need to enter a number only.")
+    raise SystemExit
+
 choose_operation = input("Choose an operation (+, -, *, /): ")
 
 if choose_operation in ("+", "-", "*", "/"):
@@ -12,6 +23,9 @@ if choose_operation in ("+", "-", "*", "/"):
     elif choose_operation == "*":
         print(first_number * second_number)
     elif choose_operation == "/":
-        print(round(first_number / second_number, 2))
+        if second_number == 0:
+            print("Cannot divide by zero.")
+        else:
+            print(round(first_number / second_number, 2))
 else:
-    print("you need to choose an operation from (+, -, *, /) only.")
+    print("You need to choose an operation from (+, -, *, /) only.")
